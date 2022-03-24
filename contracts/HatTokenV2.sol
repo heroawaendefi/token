4d87e2fd34cd6a58a8572798d1e2795a67282517
Antonidas Yang <contact@heroawaken.io>
2022-03-24 18:05:08 +0800

    hat v2 published

--- contracts/HatTokenV2.sol
+++ contracts/HatTokenV2.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
@@ -26,10 +27,10 @@ contract HatTokenV2 is
 
     mapping(address => bool) _blockSenders;
     mapping(address => bool) _blockReceivers;
+    mapping(address => bool) _liquidityFrees;
 
     /**
-     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
-     * account that deploys the contract.
+     * @dev Set liquidity fee and keeper
      *
      * See {ERC20-constructor}.
      */
@@ -45,47 +46,19 @@ contract HatTokenV2 is
         _mint(owner(), 10**9 * 10**18);
     }
 
-    /**
-     * @dev Pauses all token transfers.
-     *
-     * See {ERC20Pausable} and {Pausable-_pause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function pause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "Hero Awaken Token: must have pauser role to pause"
-        );
-        _pause();
-    }
-
-    /**
-     * @dev Unpauses all token transfers.
-     *
-     * See {ERC20Pausable} and {Pausable-_unpause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function unpause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            "Hero Awaken Token: must have pauser role to unpause"
-        );
-        _unpause();
-    }
-
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal virtual override(ERC20, ERC20Pausable) {
-        require(!_blockSenders[from], "Hero Awaken Token: the sender has been blocked");
-        require(!_blockReceivers[to], "Hero Awaken Token: the receiver has been blocked");
+        require(
+            !_blockSenders[from],
+            "Hero Awaken Token: the sender has been blocked"
+        );
+        require(
+            !_blockReceivers[to],
+            "Hero Awaken Token: the receiver has been blocked"
+        );
 
         super._beforeTokenTransfer(from, to, amount);
     }
@@ -109,7 +82,11 @@ contract HatTokenV2 is
         return (_blockSenders[user], _blockReceivers[user]);
     }
 
-    function blockUser(address user, bool can_send, bool can_receive) public onlyOwner {
+    function blockUser(
+        address user,
+        bool can_send,
+        bool can_receive
+    ) public onlyOwner {
         _blockSenders[user] = can_send;
         _blockReceivers[user] = can_receive;
     }
@@ -125,6 +102,10 @@ contract HatTokenV2 is
 
     function setLiquidityKeeper(address keeper) public onlyOwner {
         _liquidityKeeper = keeper;
+    }
+
+    function addLiquidityFree(address user) public onlyOwner {
+        _liquidityFrees[user] = true;
     }
 
     /**
@@ -146,10 +127,14 @@ contract HatTokenV2 is
         address to,
         uint256 amount
     ) internal override {
-        uint256 fee = amount.mul(_liquidityFee).div(1000);
-        amount -= fee;
+        if (_liquidityFrees[from] || _liquidityFrees[to]) {
+            super._transfer(from, to, amount);
+        } else {
+            uint256 fee = amount.mul(_liquidityFee).div(1000);
+            amount -= fee;
 
-        super._transfer(from, to, amount);
-        super._transfer(from, _liquidityKeeper, fee);
+            super._transfer(from, to, amount);
+            super._transfer(from, _liquidityKeeper, fee);
+        }
     }
 }
